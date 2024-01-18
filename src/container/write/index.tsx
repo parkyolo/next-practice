9a862e7932d9035c67db049d4ab2337ddba0c0dc
jyp <jiyounge628@gmail.com>
2024-01-18 11:11:26 +0900

    fix: 글 등록 api url 수정

--- src/container/write/index.tsx
+++ src/container/write/index.tsx
@@ -3,13 +3,13 @@ const Write = () => {
     <div>
       <h4>글작성</h4>
       {/* /api/test 이 url로 post 요청이 감 */}
-      <form action="/api/article/list" method="GET">
+      <form action="/api/post/list" method="GET">
         <button type="submit">모든 문서 가져오기 버튼</button>
       </form>
       <form action="/api/time" method="GET">
         <button type="submit">현재시간 버튼</button>
       </form>
-      <form action="/api/article/new" method="POST">
+      <form action="/api/post/new" method="POST">
         <input type="title" name="title" placeholder="제목" />
         <input type="content" name="content" placeholder="내용" />
         <button type="submit">글 발행</button>
