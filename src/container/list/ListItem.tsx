2b009b98686dce30219b0cc70c925f30476f39f5
jyp <jiyounge628@gmail.com>
2024-01-18 11:27:29 +0900

    feat: 글 삭제 애니메이션 추가

--- src/container/list/ListItem.tsx
+++ src/container/list/ListItem.tsx
@@ -12,18 +12,27 @@ const ListItem = (props: { result: string }) => {
         const id: string = _id.toString();
 
         return (
-          <div className="list-item" key={id}>
+          <div className="list-item " key={id}>
             <Link href={`/detail/${id}`}>{title}</Link>
             <Link href={`/edit/${id}`} className="list-btn">
               ✏️
             </Link>
             <button
-              onClick={() => {
+              onClick={(e) => {
                 fetch('/api/post/delete', {
                   method: 'DELETE',
                   body: id,
                 }).then(() => {
-                  console.log('삭제 완료');
+                  const eventTarget = e.target as HTMLElement;
+                  const parentElement =
+                    eventTarget.parentElement as HTMLButtonElement;
+                  // 상위 요소(글 박스)의 opacity를 0으로
+                  parentElement.style.opacity = '0';
+                  // 1초 후(opacity가 0이 된 후) 상위 요소의 display를 none으로
+                  setTimeout(
+                    () => (parentElement.style.display = 'none'),
+                    1000
+                  );
                 });
               }}
             >
